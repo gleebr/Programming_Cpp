7ec790a8b0e7a4757d81a52f8fcfc26f5a54f989
Глеб <glebruban2007@gmail.com>
2026-03-20 10:53:27 +0200

    2Lab3(new)

--- 2Lab-3/2Lab-3/2Lab-3.cpp
+++ 2Lab-3/2Lab-3/2Lab-3.cpp
@@ -1,20 +1,41 @@
 ﻿#include <iostream>
 #include "Product.h"
+#pragma comment(lib, "Productlib.lib")
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
     std::vector<Ingredient>composition1 = { Ingredient("сахар", 150.0), Ingredient("яйца", 2.0), Ingredient("масло", 200.0), Ingredient("мука", 500.0) };
-    Product product("Вафли", "Десерт", 400.0, 30, "Магазин: ...", composition1);
+    Product product1("Вафли", "Десерт", 400.0, 30, "Магазин: ...", composition1);
 
-    std::vector<std::string>forbidden = { "консервант", "сахар" };
+    std::vector<Ingredient> forbidden = {Ingredient("сахар", 100.0), Ingredient("соль", 0.0), Ingredient("консервант", 0.0)};
     Commission commission("Ул. Зелёная, 5", forbidden, 10);
 
-    product.Display();
+    product1.Display();
     commission.Display();
 
-    commission.check(product);
+    commission.check(product1);
+
+    std::cout << "================================================================" << std::endl;
+
+    std::vector<Ingredient>composition2 = { Ingredient("сахар", 50.0), Ingredient("яйца", 3.0),Ingredient("мука", 500.0) };
+    Product product2("Блины", "Десерт", 200.0, 5, "Магазин: ...", composition2);
+
+    product2.Display();
+    commission.Display();
+
+    commission.check(product2);
+
+    Product product3 = product1 + product2;
+    product3.Display();
+
+    Product product4 = product1 - product2;
+    product4.Display();
+
+    product2 -= product1;
+    product2.Display();
+
 
     return 0;
 }
--- Lab-1/Lab-1/Lab-1.cpp
+++ Lab-1/Lab-1/Lab-1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include "Product.h"
+#pragma comment(lib, "Productlib.lib")
 
 int main()
 {
@@ -26,6 +28,15 @@ int main()
 
 	std::cout << "float занимает " << sizeof(float) * 8 << " бита и принимает значения в диапазоне " << LowerLimitForFloat << " - " << UpperLimitForFloat << std::endl;
 	std::cout << "double занимает " << sizeof(double) * 8 << " бита и принимает значения в диапазоне " << LowerLimitForDouble << " - " << UpperLimitForDouble << std::endl;
+
+	std::vector<Ingredient>composition1 = { Ingredient("сахар", 150.0), Ingredient("яйца", 2.0), Ingredient("масло", 200.0), Ingredient("мука", 500.0) };
+	Product product1("Вафли", "Десерт", 400.0, 30, "Магазин: ...", composition1);
+
+	std::vector<Ingredient> forbidden = {Ingredient("сахар", 100.0), Ingredient("соль", 0.0), Ingredient("консервант", 0.0)};
+	Commission commission("Ул. Зелёная, 5", forbidden, 10);
+
+	product1.Display();
+	commission.Display();
 
 	return 0;
 }
